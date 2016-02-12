ce52af882867caa7045c2a3bc4b6dfc14cb5e787
Hugo Ferreira <hferreira@neosbybold.com>
2016-02-12 20:41:35 +0000

    Versao Inicial

---
+++ neos/odoo9/centos7/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# set odoo database host, port, user and password
+: ${PGHOST:=$DB_PORT_5432_TCP_ADDR}
+: ${PGPORT:=$DB_PORT_5432_TCP_PORT}
+: ${PGUSER:=${DB_ENV_POSTGRESQL_USER:='postgres'}}
+: ${PGPASSWORD:=$DB_ENV_POSTGRESQL_PASSWORD}
+export PGHOST PGPORT PGUSER PGPASSWORD
+
+case "$1" in
+	--)
+		shift
+		exec openerp-server "$@"
+		;;
+	-*)
+		exec openerp-server "$@"
+		;;
+	*)
+		exec "$@"
+esac
+
+exit 1
